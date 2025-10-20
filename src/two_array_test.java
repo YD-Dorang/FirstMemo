fc2af3f500ce5b1ec796242478ecdaf6b55127ab
HYD <kld9223@naver.com>
2025-10-20 16:31:51 +0900

    2차원 배열 간단학습

---
+++ src/two_array_test.java
@@ -0,0 +1,44 @@
+public class two_array_test {
+    static void main() {
+        
+        // 1. 2차원 배열
+        // 첨자 두 개를 사용하는 배열로 같은 데이터형의 변수가 행(row)과 열(column)을 나타내는데
+        // 첫 번째 첨자는 행을, 두번째 첨자는 열을 나타낸다
+        // 형식
+        // 자료형 [][]변수명 / 자료형 변수명 [][] / 자료형[] 변수명[]
+        // 배열명 참조변수가 가리키는 배열은 몇행인지만 정해지면 배열선언이 가능하기때문에
+        // 행의 정보만 있으면 배열 생성이 가능하다.
+        // 행마다 열의 개수가 다른행렬인 비정방형 배열이 가능하다.
+        // 전체 요소 출력시 중첩for문 사용
+        
+        // 2차원 배열 만들기
+        // (1) 2차원 배열 선언 (레퍼런수 변수 선언)
+        // int[][] intArray;
+        // char charArray[][];
+        // double[] doubleArray[];
+        
+        // (2) 2차원 배열 생성
+        // int intArray[][] = new int[2][5];
+        // char charArray[][] = new char[5][5];
+        // double[] doubleArray[] = new double[5][2];
+
+        // (3) 2차원 배열 선언, 생성, 초기화
+        // int intArray[][] = {{0,1,2},{3,4,5},{6,7,8}};
+        // char charArray[][] = {{'a','b','c'},{'d','e','f'}};
+        // double doubleArray[][] = {{0.01,0.02},{0.03,0.04}};
+
+        // int i[][] = new int[2][5];
+        // int size1 = i.length; //2
+        // int size2 = i[0].length // 5
+        // int size3 = i[1].length; // 5
+
+        // i.length-> 배열의 행의 개수
+        // i[n].length는 n번째 행의 열의 개수
+
+
+
+
+
+
+    }
+}
